8ad21365521e385f2b33d2b88867490489952fa2
Kevin Naserwan <kevinnaserwan@gmail.com>
2023-03-26 16:16:49 +0700

    late

--- bin/variabel.dart
+++ bin/variabel.dart
@@ -1,5 +1,21 @@
 void main(List<String> args) {
-  String nama = 'Kevin';
-  int umur = 19;
-  print('Nama Saya ' + nama + ' umur saya ' + umur.toString() + ' tahun');
+  var alamat = 'Prabumulih';
+  final nama = 'Kevin';
+
+  // Const dan Final
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  array1[0] = 10;
+  print(array2);
+
+  //Late
+  late var value = getValue();
+  print('Variabel sudah dibuat');
+  print(value);
+}
+
+String getValue() {
+  print('getValue() dipanggil');
+  return 'Kevin';
 }
